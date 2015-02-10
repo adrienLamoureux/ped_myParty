107656e9df9b8a7e92dc48ac4c350d92678e4ec8
Adrien Lamoureux <adlamour@emi.u-bordeaux1.fr>
2015-02-10 11:52:07 +0100

    first archi

---
+++ server/server.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// Module dependencies.
+var application_root = __dirname,
+    express = require('express'), //Web framework
+    path = require('path'), //Utilities for dealing with file paths
+    bodyParser  = require('body-parser'),
+    mongoose = require('mongoose');
+
+//Create server
+var app = express();
+
+// Configure server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(application_root ,'../client/app/')));
+//Show all errors in development
+
+//Start server
+var port = 4711;
+app.listen(port, function () {
+    'use strict'
+    console.log('Express server listening on port %d in %s mode', port, app.settings.env)
+});
+
+mongoose.connect('mongodb://localhost:27017/ped_myParty');
+
+
