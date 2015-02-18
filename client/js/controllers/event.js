278b34b37655452d5158fa029a1cb07a929950bc
benjaminLetourneau <benjamin.letourneau.13@gmail.com>
2015-02-18 14:03:58 +0100

    1ere Ebauche de la page d'accueil

--- client/js/controllers/event.js
+++ client/js/controllers/event.js
@@ -1,4 +1,25 @@
 //EventController
-app.controller('EventCtrl', ['$scope', 'Event', function ($scope, Event){
+app.controller('EventCtrl', ['$scope', 'Event', 'EventByOrganizerId', function ($scope, Event, EventByOrganizerId){
+
+	$scope.events = {};
+	$scope.eventsByOrganizerId = {};
+
+	$scope.getEvents = function (){
+	/*	$scope.events = Event.querry().then(function (){
+			alert ('Event : Requete SUCCESS !!!');
+		}, function (){
+			alert ('Event : erreur pendant la requete !!!');
+		});*/
+	};
+
+	$scope.getEventsByOrganizerId = function (id){
+		$scope.events = EventByOrganizerId.querry(id).then(function (){
+			alert ('EventByOrganizerId : Requete SUCCESS !!!');
+		}, function (){
+			alert ('EventByOrganizerId : erreur pendant la requete !!!');
+		});
+	};
+
+
 
 }]);
