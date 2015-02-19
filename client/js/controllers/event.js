4366085cb05ce6f9883279a90b44d364a6e56694
tsollaud <timothee.sollaud@etu-bordeaux.fr>
2015-02-19 16:14:10 +0100

    S1-EVN02; edition mode of eventForm

--- client/js/controllers/event.js
+++ client/js/controllers/event.js
@@ -1,4 +1,12 @@
 //EventController
-app.controller('EventCtrl', ['$scope', function ($scope){
-	$scope.formBtnSubmitLabel = 'Créer';
+app.controller('EventCtrl', ['$scope', '$routeParams', 'Event', function ($scope, $routeParams, Event){
+	
+	//URL event argument
+	if(angular.isDefined($routeParams.id)){
+		$scope.thisEvent = Event.get({id:$routeParams.id});
+	}
+	// if not event => $scope.thisEvent = undefined
+
+	
+	
 }]);
