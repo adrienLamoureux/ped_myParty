3ee90f8aafa7e7ba2f8f267650ac79ed9d844bea
adrienLamoureux <vergil1534@gmail.com>
2015-02-22 20:35:31 +0100

    S1-OTH02; Structure files for mongo test

---
+++ server/test/unit/mongo/testUserEvent.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+var userModel = require('./../../../models.js').userModel;
+var userEvent = require('./../../../models.js').userEvent;
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test_mongodb', function(err){
+	if(err){
+		console.error("Failed to connect to MongoDB");
+		console.log(err);
+	}
+	else
+	"Successfully connection to MongoDB";
+});
+
+mongoose.connection.db.executeDbCommand({
+			dropDatabase: 1
+});
