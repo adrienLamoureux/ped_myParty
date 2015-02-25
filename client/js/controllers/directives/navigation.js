60978c6eb7fb6498903071aa35ab5cebb95f84f9
tsollaud <timothee.sollaud@etu-bordeaux.fr>
2015-02-25 14:38:23 +0100

    S1-OTH01;Addition of menu bar (connected and disconnected) mode with css

--- client/js/controllers/directives/navigation.js
+++ client/js/controllers/directives/navigation.js
@@ -1,4 +1,11 @@
 // NavigationBar Controller
 app.controller('NavigationCtrl', ['$scope', function ($scope){
+	$scope.activeMenu=0;
+	
+	$scope.userConnected=true; //TODO : gestion de la connexion
 
+	// change Menu state
+    $scope.changeActiveMenu = function(id) {
+    	$scope.activeMenu=id;
+   	};
 }]);
