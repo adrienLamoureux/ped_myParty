3e03cb3eb2a3fb043279a2e1233910f8394c3982
tsollaud <timothee.sollaud@etu-bordeaux.fr>
2015-02-26 14:12:53 +0100

    hotfix

--- client/js/controllers/directives/eventMiniature.js
+++ client/js/controllers/directives/eventMiniature.js
@@ -1,4 +1,6 @@
 // Event Miniature Directive Controller
 app.controller('EventMiniatureCtrl', ['$scope', function ($scope){
-
+	$scope.dateNotExpired=function(date){
+		return Date.parse(date)>Date.now();
+	}
 }]);
--- client/js/controllers/directives/navigation.js
+++ client/js/controllers/directives/navigation.js
@@ -2,7 +2,7 @@
 app.controller('NavigationCtrl', ['$scope', function ($scope){
 	$scope.activeMenu=0;
 	
-	$scope.userConnected=true; //TODO : gestion de la connexion
+	$scope.userConnected=false; //TODO : gestion de la connexion
 
 	// change Menu state
     $scope.changeActiveMenu = function(id) {
