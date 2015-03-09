22e17f9bd9dbcf15b9db0a99936c6148f5cca3c7
Victor <victor.Dupin.pro@gmail.com>
2015-03-09 16:56:46 +0100

    S3-PAN

--- client/js/directives/basketEvent.js
+++ client/js/directives/basketEvent.js
@@ -1,6 +1,9 @@
 // basketEvent Directive 
 app.directive('basketEvent', function (){
 	return {
+		scope : {
+			anevent : '='
+		},
 		restrict: 'E',
 		controller: 'BasketEventCtrl',
 		templateUrl: '../../partials/directives/basketEvent.html'
