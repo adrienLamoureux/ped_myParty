705b187811a1b0c32ac16807aacfc631da26922d
hiruko3 <romain3.verger3@gmail.com>
2015-03-12 10:56:16 +0100

    fix

--- client/js/controllers/home.js
+++ client/js/controllers/home.js
@@ -1,4 +1,14 @@
 // HomePage Controller
-app.controller('HomeCtrl', ['$scope', function ($scope){
+app.controller('HomeCtrl', ['$scope', 'ngProgress', 'User', '$rootScope', function ($scope, ngProgress, User, $rootScope){
 
+	ngProgress.color("#B40404");
+	ngProgress.start();
+	
+
+	var currentUserId = $rootScope.user.user_id;		
+
+	console.log(currentUserId)
+	User.create({id:currentUserId});
+
+	
 }]);
--- client/js/services/user.js
+++ client/js/services/user.js
@@ -2,6 +2,7 @@
 app.factory('User', ['$resource', function($resource){
 	return $resource('./../api/user/:id', {id: '@_id'}, {
 		'get' : {method: 'GET', isArray:false},
-  		'update' : {method: 'PUT', isArray:false}
+  		'update' : {method: 'PUT', isArray:false},
+  		'create' : {method : 'POST'}
   	});  
 }]);
