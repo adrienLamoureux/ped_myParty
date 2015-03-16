2575e05379c484ad69347786f1da6095b50665f5
tsollaud <timothee.sollaud@etu-bordeaux.fr>
2015-03-16 11:59:01 +0100

    S4-EVN;Add buttons to create / organise an event on page "mes évènements" (userEvents.html)

--- client/js/controllers/directives/eventList.js
+++ client/js/controllers/directives/eventList.js
@@ -3,4 +3,8 @@ app.controller('EventListCtrl', ['$scope', 'Event', function ($scope, Event){
 	$scope.events = Event.query(function(data){
 		$scope.events = data;
 	});
+
+	$scope.dateNotExpired=function(date){
+		return Date.parse(date)>Date.now();
+	}
 }]);
