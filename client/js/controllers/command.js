b463bde3b4e2e5ba9b47ee17b1db001fadf8b8ae
benjaminLetourneau <benjamin.letourneau.13@gmail.com>
2015-03-23 20:13:51 +0100

    S5 - HIS : Fix bug annulation commande

--- client/js/controllers/command.js
+++ client/js/controllers/command.js
@@ -1,8 +1,8 @@
 // CommandController
-app.controller('CommandCtrl', ['$scope', '$routeParams', 'Event', 'Command', 'EventImages', 'Ticket', 'CancelTicket','ngProgress', '$window', '$timeout', 'Notification', function ($scope, $routeParams, Event, Command, EventImages, Ticket, CancelTicket, ngProgress, $window, $timeout, Notification){
+app.controller('CommandCtrl', ['$scope', '$routeParams', 'Event', 'Command', 'EventImages', 'Ticket', 'CancelTicket','ngProgress', '$window', '$timeout', '$q' ,'Notification', function ($scope, $routeParams, Event, Command, EventImages, Ticket, CancelTicket, ngProgress, $window, $timeout, $q, Notification){
 	ngProgress.color("#B40404");
 	ngProgress.start();
-	
+
 	$scope.command = Command.get({id:$routeParams.id}, function (cmdData){
 		$scope.command = cmdData;
 		$scope.events = [];
@@ -36,99 +36,108 @@ app.controller('CommandCtrl', ['$scope', '$routeParams', 'Event', 'Command', 'Ev
 		ngProgress.complete();
 	});
 
+
+	/* Annulation d'une commande */
+	$scope.stat = null;
+
+	$scope.cptType = [];
+	
+	var getAllTicketsForAnEvent = function (ticketsData, eventKey){
+		var promises = [];
+		angular.forEach(ticketsData, function (ticketId, key){
+			var deferred = $q.defer();
+			promises.push(deferred);
+			CancelTicket.get({id:ticketId}, function (ticket){
+				$scope.cptType[eventKey][ticket.ticketTypeID]++;
+				deferred.resolve($scope.cptType);
+			});
+		});
+		return $q.all(promises);
+	};
+
+	var getAllCommandEvent = function (command, canceled, partiallyCanceled ){
+
+		$scope.stat = {
+			'canceled' : canceled,
+			'partiallyCanceled' : partiallyCanceled
+		};
+
+		var promises = [];
+		
+		angular.forEach(command.eventTickets, function (eventTicket, key){
+			var deferred = $q.defer();
+			promises.push(deferred);
+			Event.get({id:eventTicket.eventID}, function (evnt){
+				$scope.cptType[key] = {};
+				if (Date.parse(evnt.dateStarting) > Date.now()){
+					
+					angular.forEach(evnt.ticketsType, function (type, key2){
+						$scope.cptType[key][type.uniqueID] = 0;
+					});
+
+					getAllTicketsForAnEvent(eventTicket.tickets, key)
+						.then (function (cptT) {
+							$timeout(function (){
+								angular.forEach(evnt.ticketsType, function (tType, key3){
+									tType.sold-= $scope.cptType[key][tType.uniqueID];
+									tType.ticketLeft += $scope.cptType[key][tType.uniqueID];
+								});
+								evnt = Event.put({id:evnt._id}, evnt, function (eventData){
+									evnt = eventData;
+								});
+							}, 1500); 
+						})
+						.then (function (){
+							$scope.stat.partiallyCanceled = true;
+							if ($scope.stat.canceled == null){
+								$scope.stat.canceled = true;
+								deferred.resolve($scope.stat);
+							}
+							else {
+								$scope.stat.canceled = ($scope.stat.canceled)?true:false;	
+								deferred.resolve($scope.stat);
+							}
+						});
+				} else {
+					$scope.stat.canceled = false;	
+					deferred.resolve($scope.stat);	
+				};				
+			});
+		});
+ 		return $q.all(promises);
+	};
+
 	$scope.cancelCommand = function (idCommand){
 		ngProgress.start();
-		var canceled = true;
-		var partiallyCanceled = false;
-		var cptTicket = 0 ;
-		var cptEvent = 0;
+
 		var command = Command.get({id:idCommand}, function (commandData){
 			command = commandData;
 			
-			cptEvent = command.eventTickets.length;
-			angular.forEach(command.eventTickets, function (eventTicket, key){
-			
-				var evnt = Event.get({id:eventTicket.eventID}, function (eventData){
-					evnt = eventData;
-
-					if (Date.parse(evnt.dateStarting) > Date.now()){
-						var cptType = {};
-						
-						angular.forEach(evnt.ticketsType, function (type, key){
-							cptType[type.uniqueID] = 0;
-						});
-
-						cptTicket += eventTicket.tickets.length;
-
-						angular.forEach(eventTicket.tickets, function (ticketId, key){
-							var ticket = CancelTicket.get({id:ticketId}, function (ticketData){
-								ticket = ticketData;
-
-								cptType[ticket.ticketTypeID]++;
-								cptTicket--;
-
-								if (cptTicket == 0){
-									angular.forEach(evnt.ticketsType, function (tType, key){
-										tType.sold-= cptType[tType.uniqueID];
-										tType.ticketLeft += cptType[tType.uniqueID];
-									});
-
-									evnt = Event.put({id:evnt._id}, evnt, function (eventData){
-										evnt = eventData;
-										partiallyCanceled = true;
-										cptEvent --;
-
-										$timeout( function(){ 									
-											if (cptEvent = 0) {
-												command.canceled = canceled;
-												command.partiallyCanceled = partiallyCanceled;
-												command = Command.put ({id:idCommand}, command, function (commandData2){
-													command = commandData2;
-													if (!canceled){
-														notification5Sec("Votre commande a été partiellement annulée à cause d'un énènement en cours, vous ne serez remboursés que sur les tickets annulés.", "Annulation de commande !");	
-													}
-													else{
-														notification5Sec("Votre commande a été annulée, vous allez bientot recevoir un remboursement.", "Annulation de commande !");	
-													};
-													ngProgress.complete();
-													$window.location.href = '#/usr/cmds';		
-												}, function (err){
-													console.log (err);
-												});
-											};
-										}, 1200);
-									}, function (err){
-										console.log(err);
-									});	
-								};
-							}, function (err){
-								console.log(err);
-							});
-						});
-					} else {
-						canceled = false;
-						cptEvent --;
-						if (cptEvent = 0) {
-							command.canceled = canceled;
-							command.partiallyCanceled = partiallyCanceled;
-							command = Command.put ({id:idCommand}, command, function (commandData2){
-								command = commandData2;
-								if (!partiallyCanceled){
-									notification5Sec("Votre commande ne peut être annulée à cause d'un énènement en cours, aucun remboursement ne sera effectué.", "Annulation de commande !");	
-								}
-								else{
-									notification5Sec("Votre commande a été partiellement annulée à cause d'un énènement en cours, vous ne serez remboursés que sur les tickets annulés.", "Annulation de commande !");
-								};
-								ngProgress.complete();
-								$window.location.href = '#/usr/cmds';
-							}, function (err){
-								console.log (err);
-							});
+			getAllCommandEvent(command, null, false).then (function (stat){
+				$timeout(function (){
+					command.canceled = $scope.stat.canceled;
+					command.partiallyCanceled = $scope.stat.partiallyCanceled;
+					command = Command.put ({id:idCommand}, command, function (commandData2){
+						command = commandData2;
+						if (command.partiallyCanceled && command.canceled){
+							notification5Sec("Votre commande a été annulée, vous allez bientot recevoir un remboursement.", "Annulation de commande !");							
+						}
+						else if (!command.partiallyCanceled && !command.canceled){
+							notification5Sec("Votre commande ne peut être annulée à cause d'un énènement en cours, aucun remboursement ne sera effectué.", "Annulation de commande !");	
+						}
+						else if (!command.canceled && command.partiallyCanceled){
+							notification5Sec("Votre commande a été partiellement annulée à cause d'un énènement en cours, vous ne serez remboursés que sur les tickets annulés.", "Annulation de commande !");	
+						}
+						else {
+							console.log ("erreur pas possible ")
 						};
-					};
-				}, function (err){
-					console.log(err);
-				});
+						
+						ngProgress.complete();
+						$window.location.href = '#/usr/cmds';
+					}, function (err){
+						console.log (err);
+					});
+				}, 1500);
 			});
 		}, function (err){
 			console.log(err);
