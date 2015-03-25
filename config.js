38ec0772b7a863dc1ab31173f7ac1a0fffb4c03c
adrienLamoureux <vergil1534@gmail.com>
2015-03-25 17:30:32 +0100

    S5; Enhance config

---
+++ config.js
@@ -0,0 +1,13 @@
+var mongoAdress = "";
+var serverPort = "";
+var apiID = "";
+var userRefApiID = "";
+var stripeKey = "";
+
+module.exports = {
+	mongoAdress = mongoAdress,
+	serverPort = serverPort,
+	apiID = apiID,
+	userRefApiID = userRefApiID,
+	stripeKey = stripeKey
+}
